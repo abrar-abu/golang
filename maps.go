093d96b786b499a878ef0e97d1e0d4f29a78f196
abrar-abu <abrar5711@gmail.com>
2020-12-13 14:00:29 +0530

    Add structs to maps

--- maps.go
+++ maps.go
@@ -2,13 +2,28 @@ package main
 
 import "fmt"
 
+type person struct {
+    name string
+    age  int
+}
+
 func main() {
+
+	persons := make(map[int]person)
+    persons[0] = person{"Bob", 20}
+    persons[1] = person{"Alice", 30}
+    persons[2] = person{"Fred", 40}
+	fmt.Println("Map of Persons ",persons) 
+	for key, value := range persons  {
+		fmt.Println("key is ",key," Value {name:", value.name,", age:",value.age,"}")
+	}
 
 	// Confusion about this statement
 	// var names map[int]string    // name map has int keys and string values
 	
 	// To intilise  and declare a map
-    m := make(map[string]int)
+	m := make(map[string]int)
+	fmt.Println("Empty map",m)
 
 	// Assign values
     m["k1"] = 7
